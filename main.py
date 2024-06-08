be5a97570c3eb7f61b780e7d5925bc65266e7dc3
Dlexeyn <galenko.aleksej17@gmail.com>
2024-06-08 17:10:11 +0300

    feat: add transforming hex list to text

--- main.py
+++ main.py
@@ -18,8 +18,7 @@ def save_file(text):
     breathe_speed.text = "2"
 
     color_picture = ET.SubElement(root, "ColorPicture")
-    color_picture.text = ("A449A3,0000FF,0000FF,0000FF,0000FF,0000FF,0000FF,0000FF,0000FF,0000FF,0000FF,0000FF,0000FF,"
-                          "A449A3,A449A3,000000,000000,000000")
+    color_picture.text = (text + ",000000,000000,000000")
 
     # Создание дерева XML
     tree = ET.ElementTree(root)
@@ -33,13 +32,28 @@ def rgb_to_hex(rgb):
     return '%02x%02x%02x' % rgb
 
 
+def color_hex_to_text(colors_hex):
+    text = ",".join(colors_hex)
+
+    # Разделение строки на подстроки
+    substrings = text.split(',')
+    reversed_substrings = reversed(substrings)
+    reversed_text = ','.join(reversed_substrings)
+
+    all_text = text + ',' + reversed_text + ',' + substrings[0]
+    print(all_text)
+    return all_text
+
+
 def main():
     wall = sys.argv[1] if len(sys.argv) > 1 else "No Path to Image"
 
     print(f"Wallpaper: {wall}")
     colors = colorgram.extract(wall, 7)
 
-    colors_hex = [rgb_to_hex(color) for color in colors]
+    colors_hex = [rgb_to_hex(color.rgb) for color in colors]
+
+    save_file(color_hex_to_text(colors_hex))
 
     for i, color in enumerate(colors):
         print(f"color {i + 1} - {color.rgb}")
